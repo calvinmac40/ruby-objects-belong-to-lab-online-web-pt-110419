268fc4eba6026fdfd99ee2f43976f91b2b75dabe
Calvin McDowell <calvinmac40@hotmail.com>
2020-01-03 00:19:21 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,12 @@
 class Post 
   attr_accessor :title, :author
+  
+  def initialize
+    @title = title
+    @author = author
+  end
  
- def self.author
-   self.Author = author
- end
+# def self.author
+#   self.Author = author
+# end
 end
